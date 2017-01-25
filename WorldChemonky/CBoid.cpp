67045c7ec2a46adef2b997f504acbd6a34238277
chemonky <luricardorod@gmail.com>
2017-01-24 19:02:18 -0600

    persue, evade, wander1, wander2

--- WorldChemonky/CBoid.cpp
+++ WorldChemonky/CBoid.cpp
@@ -1,41 +1,104 @@
 #include "stdafx.h"
 #include "CBoid.h"
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
 void CBoid::Init() {
+	srand(time(NULL));
 }
 
 void CBoid::Destroy() {
 }
 
 void CBoid::Update(float delta) {
-	if (Magnity(_mVelocity) > _mMaxSpeed)
-	{
-		_mVelocity = Normalize(_mVelocity) * _mMaxSpeed;
-	}
-	_mPosition = _mPosition + _mVelocity * delta;
+	
 }
 void CBoid::Render() {
 }
 
-CVector2D CBoid::Seek(CVector2D position, float forceValue) {
-	return Normalize(position - _mPosition) * forceValue;
+CVector2D CBoid::Seek(CVector2D position, float forceSeek) {
+	return Normalize(position - _mPosition) * forceSeek;
 }
 
-CVector2D CBoid::Flee(CVector2D position, float forceValue, float radiusOfForceAplication) {
+CVector2D CBoid::Flee(CVector2D position, float forceFlee, float radiusOfForceAplication) {
 	CVector2D direction = _mPosition - position;
 	if (Magnity(direction) < radiusOfForceAplication) 
 	{
-		return Normalize(direction) * forceValue;
+		return Normalize(direction) * forceFlee;
 	}
 	return CVector2D(0, 0);
 }
 
-CVector2D CBoid::Arrive(CVector2D position, float forceValue, float radiusOfForceAplication) {
-	CVector2D arriveForce = Seek(position, forceValue);
+CVector2D CBoid::Arrive(CVector2D position, float forceArrive, float radiusOfForceAplication) {
+	CVector2D arriveForce = Seek(position, forceArrive);
 	float distance = Magnity(position - _mPosition);
 	if (distance < radiusOfForceAplication)
 	{
 		arriveForce = arriveForce * (distance / radiusOfForceAplication);
 	}
 	return arriveForce;
+}
+
+CVector2D CBoid::Pursue(CVector2D position, CVector2D direction, float velocity, float timePrediction, float forcePersue)
+{
+	CVector2D futurePosition = position + (direction*velocity*timePrediction);
+	float distanceTarget = Magnity(position - _mPosition);
+	float radioPrediction = Magnity(futurePosition - position);
+
+	if (distanceTarget > radioPrediction)
+	{
+		return Normalize(futurePosition - _mPosition) * forcePersue;
+	}
+	return Normalize(futurePosition*(distanceTarget/ radioPrediction) - _mPosition) * forcePersue;
+}
+
+CVector2D CBoid::Evade(CVector2D position, CVector2D direction, float velocity, float timePrediction, float radiusOfForceAplication, float forceEvade)
+{
+	CVector2D futurePosition = position + (direction*velocity*timePrediction);
+	float distanceTarget = Magnity(position - _mPosition);
+	float radioPrediction = Magnity(futurePosition - position);
+
+	if (distanceTarget > radioPrediction)
+	{
+		if (Magnity(_mPosition-futurePosition) > radiusOfForceAplication)
+		{
+			return CVector2D(0, 0);
+		}
+		return Normalize(_mPosition - futurePosition) * forceEvade;
+	}
+	return Normalize(_mPosition - futurePosition*(distanceTarget / radioPrediction)) * forceEvade;
+}
+
+CVector2D CBoid::Wander2(float offset, float radiusNextPoint, float visionAngle, float forceWander2)
+{
+	CVector2D projectedPoint = _mPosition + (_mDirection*offset);
+	float angleBoid = atanf(_mDirection.y / _mDirection.x);
+
+	float angleInVision = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / visionAngle));
+	angleInVision = angleInVision - (visionAngle / 2);
+
+	CVector2D pointInAngleVision(cosf(angleBoid + angleInVision)*radiusNextPoint, sin(angleBoid + angleInVision)*radiusNextPoint);
+
+	CVector2D target = projectedPoint + pointInAngleVision;
+
+	return Seek(target, forceWander2);
+}
+
+CVector2D CBoid::Wander1(CVector2D SizeWorld, float radiusArrival, float timeLimitSeek, float forceWander1, float delta)
+{
+	static float x = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / SizeWorld.x));
+	static float y = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / SizeWorld.y));
+	static float timeSeek = 0;
+	static CVector2D randomTarget(x, y);
+
+	float distance = Magnity(randomTarget - _mPosition);
+	timeSeek += delta;
+
+	if (timeSeek > timeLimitSeek || distance < radiusArrival)
+	{
+		randomTarget.x = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / SizeWorld.x));
+		randomTarget.y = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / SizeWorld.y));
+	}
+	return Seek(randomTarget, forceWander1);
 }
--- WorldChemonky/CBoid.h
+++ WorldChemonky/CBoid.h
@@ -4,7 +4,8 @@
 class CBoid:public CGameObject
 {
 private:
-	CVector2D _mVelocity;
+	float _mVelocity;
+	CVector2D _mDirection;
 	float _mMaxSpeed;
 
 public:
@@ -12,12 +13,12 @@ public:
 	void Destroy();
 	void Update(float delta);
 	void Render();
-	CVector2D Seek(CVector2D position, float forceValue);
-	CVector2D Flee(CVector2D position, float forceValue, float radiusOfForceAplication);
-	CVector2D Arrive(CVector2D position, float forceValue, float radiusOfForceAplication);
-	CVector2D Pursue(CVector2D position, CVector2D direction,float velocity, float timePrediction);
-	CVector2D Evade(CVector2D position, CVector2D direction, float velocity, float timePrediction, float radiusOfForceAplication);
-	CVector2D Wander1(CVector2D SizeWorld, float radiusArrival, float timeSeek);
-	CVector2D Wander2(float offset,float radiusNextPoint, float visionAngle);
+	CVector2D Seek(CVector2D position, float forceSeek);
+	CVector2D Flee(CVector2D position, float forceFlee, float radiusOfForceAplication);
+	CVector2D Arrive(CVector2D position, float forceArrive, float radiusOfForceAplication);
+	CVector2D Pursue(CVector2D position, CVector2D direction,float velocity, float timePrediction, float forcePersue);
+	CVector2D Evade(CVector2D position, CVector2D direction, float velocity, float timePrediction, float radiusOfForceAplication, float forceEvade);
+	CVector2D Wander1(CVector2D SizeWorld, float radiusArrival, float timeLimitSeek, float forceWander1, float delta);
+	CVector2D Wander2(float offset,float radiusNextPoint, float visionAngle, float forceWander2);
 };
 
